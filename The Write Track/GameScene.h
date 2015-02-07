599b77f0b1927af5f7bb5d7ffae089475faf074f
Mitch Clutter <mclutter@pillartechnology.com>
2015-02-07 16:47:39 -0500

    Initial project creation using SpriteKit

---
+++ The Write Track/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  The Write Track
+//
+
+//  Copyright (c) 2015 Mitch Clutter. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ The Write Track/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  The Write Track
+//
+
+//  Copyright (c) 2015 Mitch Clutter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end

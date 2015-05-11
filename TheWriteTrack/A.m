a642843df84586569df9faf691576afe18b34c67
Mitch Clutter <cluttermitch@johndeere.com>
2015-05-11 08:25:23 -0400

    Spike method out to draw dots on the points of the path for debugging and design decisions.

--- TheWriteTrack/A.m
+++ TheWriteTrack/A.m
@@ -8,6 +8,7 @@
 
 #import "A.h"
 #import "LayoutMath.h"
+#import "PathInfo.h"
 #import <UIKit/UIKit.h>
 
 CGFloat const LetterLineWidth = 10;
@@ -16,11 +17,12 @@ NSString *const LetterNodeName = @"LetterNode";
 
 @implementation A
 
-- (void)moveNodeToCenter:(SKNode *)node {
+- (CGPoint)moveNodeToCenter:(SKNode *)node {
     CGPoint center = [LayoutMath centerOfMainScreen];
     center.x -= (node.frame.size.width * 0.5) - (LetterLineWidth * 0.1);
     center.y -= (node.frame.size.height - LetterLineWidth) * 0.5;
     node.position = center;
+    return center;
 }
 
 -(SKShapeNode *)createLetterPathNode {
@@ -31,8 +33,25 @@ NSString *const LetterNodeName = @"LetterNode";
     letterPathNode.strokeColor = [SKColor darkGrayColor];
     letterPathNode.fillTexture = [SKTexture textureWithImageNamed:@"TrackTexture"];
     letterPathNode.fillColor = [SKColor whiteColor];
-    [self moveNodeToCenter:letterPathNode];
+    CGPoint center = [self moveNodeToCenter:letterPathNode];
+    [self drawDotsAtCenter:center OfPath:attrStringPath.letterPath];
     return letterPathNode;
+}
+
+-(void)drawDotsAtCenter:(CGPoint)center OfPath:(CGPathRef)path {
+    PathInfo *pathInfo = [[PathInfo alloc] init];
+    NSArray *array = [pathInfo TransformPathToArray:path];
+    for (NSUInteger i = 0; i < array.count; ++i) {
+        SKShapeNode *node = [SKShapeNode shapeNodeWithCircleOfRadius:5];
+        node.fillColor = [SKColor redColor];
+        NSValue *pointValue = (NSValue *)[array objectAtIndex:i];
+        CGPoint point = [pointValue CGPointValue];
+        point.x += center.x;
+        point.y += center.y;
+        node.position = point;
+        node.zPosition = 5;
+        [self addChild:node];
+    }
 }
 
 -(SKNode *)createTrainNode {

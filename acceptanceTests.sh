9dbd3e92237c8e0c33301814562dd3ba351bd9f4
Mitch Clutter <cluttermitch@johndeere.com>
2015-09-15 13:35:31 -0400

    updating acceptance test script

--- acceptanceTests.sh
+++ acceptanceTests.sh
@@ -1,27 +1,31 @@
-#!/bin/bash
+#!/bin/bash --login
 
 FEATURE_FILES=${1}
 
+CURRENT_DIR=`pwd`
+BUILD_DIR="$CURRENT_DIR/build_acceptanceTest"
+APP_NAME="TheWriteTrack"
+
 killall 'iOS Simulator' 1&>/dev/null
+
+rm -rf $BUILD_DIR
 
 bundle install
 
 bundle exec pod install
 
-open TheWriteTrack.xcworkspace
-
-printf 'y\nTheWriteTrack\n' | bundle exec calabash-ios setup
-
-xctool -workspace TheWriteTrack.xcworkspace/ \
-	 -scheme TheWriteTrack-cal \
-	 -configuration Debug \
+xctool \
+     -workspace $APP_NAME.xcworkspace/ \
+	 -scheme $APP_NAME-cal \
+     CONFIGURATION_BUILD_DIR=$BUILD_DIR \
 	 -destination "platform=iOS Simulator,name=iPhone 6,OS=8.3" \
-	 -derivedDataPath ./build_acceptanceTest \
+     -configuration Debug \
+     -sdk iphonesimulator \
 	 clean \
 	 build
 
 bundle exec calabash-ios sim reset
 
-bundle exec "TARGET_DEVICE='iPhone 6 (iOS 8.3)' cucumber ${FEATURE_FILES}"
+bundle exec "APP_BUNDLE_PATH=$BUILD_DIR/$APP_NAME.app TARGET_DEVICE='iPhone 6 (iOS 8.3)' cucumber ${FEATURE_FILES}"
 
 killall 'iOS Simulator' 1&>/dev/null
